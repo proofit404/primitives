3e3ee4a614864bec6a6cee1f55000cfee854428e
Artem Malyshev <proofit404@gmail.com>
2021-01-02 03:08:20 +0300

    feat: implement callable return value #3

--- src/_primitives/callable.py
+++ src/_primitives/callable.py
@@ -1,6 +1,12 @@
 class Callable:
     """An object appropriate to fake functions and methods."""
 
+    def __init__(self, *args):
+        if args:
+            self.return_value = args[0]
+        else:
+            self.return_value = None
+
     def __call__(self):
         """Return predefined value."""
-        pass
+        return self.return_value
